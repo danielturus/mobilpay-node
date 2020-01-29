2aee0c81f7504e0ef1c7c80db44fcbe0e84b6c3f
Daniel T <daniel.turus@evozon.com>
2020-01-29 15:40:10 +0200

    dsadsa

--- dist/es5/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var MobilPay = require('./lib/MobilPay');
-var MerchantResponse = require('./lib/merchant/Response');
-var constants = require('./lib/constants');
-
-module.exports.Mobilpay = MobilPay;
-module.exports.MerchantResponse = MerchantResponse;
-module.exports.constants = constants;
